27f62f7d104d4fec05875893cc95d37e5428b04f
Beanii <chlqls212@gmail.com>
2025-11-01 01:06:47 +0900

    first commit

---
+++ src/main/java/com/fourspoons/mikkureomi/MikkureomiApplication.java
@@ -0,0 +1,13 @@
+package com.fourspoons.mikkureomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikkureomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikkureomiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourspoons/mikkureomi/MikkureomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourspoons.mikkureomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikkureomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
