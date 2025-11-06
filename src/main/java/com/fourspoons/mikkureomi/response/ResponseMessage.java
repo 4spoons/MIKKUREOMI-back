93a939260ada84818176f23ca3d51d2bcabb5e27
D <deilee0913@gmail.com>
2025-11-06 20:42:28 +0900

    feat(notification): 핵심 알림 도메인 및 API 스펙 정의
    - Notification Entity, Category ENUM, Response DTO 추가
    - 알림 관련 기본 Repository 및 응답/에러 메시지 등록

--- src/main/java/com/fourspoons/mikkureomi/response/ResponseMessage.java
+++ src/main/java/com/fourspoons/mikkureomi/response/ResponseMessage.java
@@ -32,7 +32,13 @@ public enum ResponseMessage {
     DELETE_DAILY_REPORT_SUCCESS("데일리 리포트가 삭제되었습니다."),
 
     // Recommendation
-    RECOMMENDATION_FETCH_SUCCESS("영양성분 권장섭취량 조회가 완료되었습니다.");
-    
+    RECOMMENDATION_FETCH_SUCCESS("영양성분 권장섭취량 조회가 완료되었습니다."),
+
+    // Notification
+    GET_NOTIFICATION_SUCCESS("알림 목록을 성공적으로 조회했습니다."),
+    ISREAD_UPDATE_SUCCESS("알림 읽음 상태를 성공적으로 변경했습니다."),
+    REMAINING_MESSAGE_NUMBER_SUCCESS("읽지 않은 알림 개수를 성공적으로 조회했습니다.");
+
+
     private final String message;
 }
