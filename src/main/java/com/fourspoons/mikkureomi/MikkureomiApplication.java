852a25e8ab0141c50fc9fd1159b7637991710254
D <deilee0913@gmail.com>
2025-11-06 20:43:38 +0900

    feat(notification): 결식 위험 알림 스케줄러 및 서비스 로직 구현
    - NotificationService에 48시간 결식 위험 체크 로직 구현
    - NotificationScheduler 등록 및 주기적 알림 발송 설정
    - 관련 Repository에 필요한 조회 쿼리 추가

--- src/main/java/com/fourspoons/mikkureomi/MikkureomiApplication.java
+++ src/main/java/com/fourspoons/mikkureomi/MikkureomiApplication.java
@@ -3,9 +3,11 @@ package com.fourspoons.mikkureomi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class MikkureomiApplication {
 
 	public static void main(String[] args) {
